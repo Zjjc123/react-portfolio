696bcf5fda73681475a0d00575fe551bde277fbe
Jason Zhang <zjjc123@hotmail.com>
2020-04-11 13:16:59 -0700

    Profile Picture

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,15 +1,19 @@
-import React from "react"
+import React from "react";
 import ProjectCard from "../components/Card.js";
 import Jumbotron from "react-bootstrap/Jumbotron";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
+import Image from "react-bootstrap/Image";
+
+import Background from "../img/jumbotronSquare.jpg";
 
 function Projects(props) {
     return (
         <Jumbotron className="bg-transparent jumbotron-fluid p-0">
+            <Image id = "profile" className = "py-5" src={Background} roundedCircle fluid/>
             <Container fluid>
-                <Row className="justify-content-center py-5">
+                <Row className="justify-content-center" >
                     <Col>
                         <h1 className="display-1 font-weight-bolder" >Jason Zhang</h1>
                         <h3 className="display-4 font-weight-light">My Projects</h3>
@@ -17,7 +21,7 @@ function Projects(props) {
                     </Col>
                 </Row>
             </Container>
-            <Container fluid>
+            <Container fluid className = "py-5">
                 <Row>
                     <Col style={{ display: 'flex', justifyContent: 'center' }}>
                         <ProjectCard
@@ -40,7 +44,7 @@ function Projects(props) {
                             ]}
                         />
                     </Col>
-                    <Col style = {{display: 'flex', justifyContent: 'center'}}>
+                    <Col style={{ display: 'flex', justifyContent: 'center' }}>
                         <ProjectCard
                             title="Vitality"
                             description="Mobile Fitness App built on Android Studio that provides a variety of health-related functionalities like heart rate monitoring and fitness counters. Complete with a minimalistic UI, profile settings, and pinned notifications."
