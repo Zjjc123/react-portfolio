d9a0e249dabfa8fe7e612c55fac0c144869c7f47
Jason Zhang <zjjc123@hotmail.com>
2020-04-16 10:00:06 -0700

    Loading Screen + Reformat

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-import {HashRouter, Route} from 'react-router-dom';
+import { HashRouter, Route } from 'react-router-dom';
 import Projects from './pages/Projects.js';
 import About from './pages/About.js';
 import Gallery from './pages/Gallery.js';
@@ -14,39 +14,43 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
 
-function App() {
-  return (
-    <HashRouter basename={process.env.PUBLIC_URL}>
-      <div className="App">
-        <Navbar bg="transparent" expand="lg">
-          <Navbar.Brand href="/">
-            <img
-              src= {logo}
-              width="30"
-              height="30"
-              className="d-inline-block align-top"
-              alt="Logo"
-            />
-          </Navbar.Brand>
-          <Navbar.Brand href="/">Jason Zhang</Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="ml-auto">
-              <Nav.Link href="/#">Projects</Nav.Link>
-              <Nav.Link href="/#/about">About</Nav.Link>
-              <Nav.Link href="/#/gallery">Gallery</Nav.Link>
-            </Nav>
-          </Navbar.Collapse>
-        </Navbar>
+class App extends Component {
+  render() {
+    return (
+      <div id="loading">
+        <HashRouter basename={process.env.PUBLIC_URL} >
+          <div className="App">
+            <Navbar bg="transparent" expand="lg">
+              <Navbar.Brand href="/">
+                <img
+                  src={logo}
+                  width="30"
+                  height="30"
+                  className="d-inline-block align-top"
+                  alt="Logo"
+                />
+              </Navbar.Brand>
+              <Navbar.Brand href="/">Jason Zhang</Navbar.Brand>
+              <Navbar.Toggle aria-controls="basic-navbar-nav" />
+              <Navbar.Collapse id="basic-navbar-nav">
+                <Nav className="ml-auto">
+                  <Nav.Link href="/#">Projects</Nav.Link>
+                  <Nav.Link href="/#/about">About</Nav.Link>
+                  <Nav.Link href="/#/gallery">Gallery</Nav.Link>
+                </Nav>
+              </Navbar.Collapse>
+            </Navbar>
 
-        <Route exact path= {'/'} component={Projects} />
-        <Route path= {'/about'} component={About} />
-        <Route path= {'/gallery'} component={Gallery} />
+            <Route exact path={'/'} component={Projects} />
+            <Route path={'/about'} component={About} />
+            <Route path={'/gallery'} component={Gallery} />
 
-        <Footer/>
+            <Footer />
+          </div>
+        </HashRouter>
       </div>
-    </HashRouter>
-  );
+    );
+  }
 }
 
 export default App;
