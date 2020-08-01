fcccf8d99155be75a0ead0188b872dc68d9f2e2d
Jason Zhang <zjjc123@hotmail.com>
2020-07-31 18:52:58 -0700

    Fixing Package and Routing

--- src/App.js
+++ src/App.js
@@ -45,7 +45,7 @@ class App extends Component {
             </Navbar>
 
             <Route exact path={'/'} component={Home} />
-            <Route exact path={'/projects'} component={Projects} />
+            <Route path={'/projects'} component={Projects} />
             <Route path={'/about'} component={About} />
             <Route path={'/gallery'} component={Gallery} />
 
