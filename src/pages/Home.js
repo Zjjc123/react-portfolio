737916b7f8a20d934f137f97bc01014c4ebfabfe
Jason Zhang <zjjc123@hotmail.com>
2020-08-03 13:07:04 -0700

    Text and Build Fixes

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,5 @@
 import React, { useEffect, useRef } from "react";
 import Container from "react-bootstrap/Container";
-import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 
 import { TweenMax, Power3 } from 'gsap';
