b4de6a90109528be83c6c7dda664efcc9d467808
Zjjc123 <zjjc123@hotmail.com>
2020-08-08 21:08:30 +0000

    Deploying to gh-pages from  @ d3f7f3b61caf493b719b2ad1c3b3f7b0d96ba80e 🚀

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.8020043e774bcfa63b0bc373b58c0b33.js"
+  "/precache-manifest.18c6f313be1a7622bf7e6337a539d2ef.js"
 );
 
 self.addEventListener('message', (event) => {
