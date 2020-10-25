7459d9ea8e45873b6b566a407eb058fc2b2eddfd
Jason Zhang <zjjc123@hotmail.com>
2020-10-24 20:37:11 -0700

    Debounce Resize

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import './styles/App.scss';
 import Header from './components/Header';
 import PageRouter from './components/Router';
@@ -6,12 +6,39 @@ import PageRouter from './components/Router';
 import gsap from 'gsap';
 import Navigation from './components/Navigation';
 
+function debounce(fn, ms) {
+  let timer;
+  return () => {
+    clearTimeout(timer);
+    timer = setTimeout(() => {
+      timer = null;
+      fn.apply(this, arguments);
+    })
+  }
+}
+
 const App = () => {
+  const [dimensions, setDimensions] = useState({
+    height: window.innerHeight,
+    width: window.innerWidth
+  });
   useEffect(() => {
-    let vh = window.innerHeight * 0.01;
+    let vh = dimensions.height * 0.01;
     document.documentElement.style.setProperty("--vh", `${vh}px`);
 
+    const debouncedHandleResize = debounce (function handleResize() {
+      setDimensions({
+        height: window.innerHeight,
+        width: window.innerWidth
+      });
+    }, 1000)
+
+    window.addEventListener("resize", debouncedHandleResize);
     gsap.to('body', 0, { css: { visibility: "visible" } });
+
+    return () => {
+      window.removeEventListener("resize", debouncedHandleResize);
+    }    
   })
   return (
     <>
