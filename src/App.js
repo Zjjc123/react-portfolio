6fe18b6c87929a97672fe41ee0a7ec9a1aeb3bc4
Jason Zhang <zjjc123@hotmail.com>
2020-10-24 17:18:14 -0700

    Basic Landing Animation

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './styles/App.scss';
-
 import Header from './components/Header';
 import PageRouter from './components/Router';
 
+import gsap from 'gsap';
+
 const App = () => {
-    return (
+  useEffect(() => {
+    let vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty("--vh", `${vh}px`);
+
+    gsap.to('body', 0, { css: { visibility: "visible" } });
+  })
+  return (
     <div className="App">
       <Header />
       <PageRouter />
--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -1,43 +1,62 @@
-import React from "react";
-import Container from "react-bootstrap/Container";
+import React, { useEffect, useState } from "react";
+import { ReactComponent as RightArrow } from '../assets/arrow-right.svg';
+import IntroOverlay from "../components/IntroOverlay";
+import gsap from 'gsap';
 
-import { motion } from 'framer-motion'
+const tl = gsap.timeline();
 
-const transition = { duration: 0.6, ease: [0.6, 0.01, -0.05, 0.9] }
+const homeAnimation = (completeAnimation) => {
+    tl.from('.line span', 1.8, {
+        y: 100,
+        ease: 'power4.out',
+        delay: 1,
+        skewY: 7,
+        stagger: {
+            amount: 0.3
+        }
+    }).to('.intro-overlay', 1.6, {
+        height: 0,
+        ease: 'expo.inOut'
+    }).to('.intro-overlay', 0, {
+        css: { display: 'none' },
+        onComplete: completeAnimation
+    });
+}
 
 function Landing() {
+    const [animationComplete, setAnimationComplete] = useState(false);
+
+    const completeAnimation = () => {
+        setAnimationComplete(true);
+    }
+
+    useEffect(() => {
+        homeAnimation(completeAnimation);
+    }, [])
+
     return (
-        <motion.div>
-            <Container fluid className="titleBackground">
-                    <motion.div
-                        initial={{ opacity: 0, scale: 1.1 }}
-                        animate={{ opacity: 1, scale: 1 }}
-                        exit={{ opacity: 0, scale: 1.1 }}
-                        transition={transition}>
-                        <video className="background-video" loop autoPlay muted>
-                            <source src={process.env.PUBLIC_URL + "/videos/landing.mp4"} type="video/mp4" />
-                        </video>
-                    </motion.div>
-                <div className="heading">
-                    <motion.h1
-                        initial={{ opacity: 0, y: -10 }}
-                        animate={{ opacity: 1, y: 0 }}
-                        exit={{ x: "-150%" }}
-                        transition={transition}
-                        className="homeTitle1 not-selectable" >JASON ZHANG</motion.h1>
-                    <motion.h4 exit={{ x: "-150%" }}
-                        initial={{ opacity: 0, y: -10 }}
-                        animate={{ opacity: 1, y: 0 }}
-                        transition={{ delay: 0.2, ...transition }}
-                        className="homeTitle2 not-selectable">CREATIVE</motion.h4>
-                    <motion.h3 exit={{ x: "-150%" }}
-                        initial={{ opacity: 0, y: -10 }}
-                        animate={{ opacity: 1, y: 0 }}
-                        transition={{ delay: 0.4, ...transition }}
-                        className="font-weight-light homeTitle3 not-selectable">EASTLAKE 2021</motion.h3>
-                </div>
-            </Container>
-        </motion.div >
+        <>
+            {animationComplete === false ? <IntroOverlay /> : ""}
+            <div className="full">
+                <section className="main">
+                    <div className="container">
+                        <div className='row'>
+                            <h2>
+                                <div className="line">
+                                    <span>Welcome to my portfolio!</span>
+                                </div>
+                                <div className="line">
+                                    <span>I'm Jason Zhang</span>
+                                </div>
+                            </h2>
+                            <div className="btn-row">
+                                <a href="/">about me <RightArrow /> </a>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+            </div>
+        </>
     )
 }
 
