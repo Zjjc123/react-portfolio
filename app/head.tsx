8f0bbc7be18c1e1f699a9017bacfd479fdd90e22
Jason Zhang <zjjc123@hotmail.com>
2023-01-01 22:45:32 -0800

    Upgrading to Next.js 13 app dir

---
+++ app/head.tsx
@@ -0,0 +1,10 @@
+export default async function Head() {
+    return (
+        <>
+            <title>Jason Zhang Portfolio</title>
+            <meta name="description" content="next.js portfolio" />
+            <link rel="icon" href="/favicon.ico" />
+            <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        </>
+    )
+}
