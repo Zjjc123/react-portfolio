35b09388d5e3b191eac680c34ae38e21414052ce
Jason Zhang <zjjc123@hotmail.com>
2023-05-07 19:07:12 -0700

    More blogs

--- components/Blog.tsx
+++ components/Blog.tsx
@@ -8,10 +8,25 @@ export default function Blog({}: Props) {
   return (
     <>
       <BlogEntry date="05/07/2021">
-        <P>{'The churning of a near empty lemonade machine. A low fan hums as he worked quietly. The closing shift'}</P>
+        <P>
+          {
+            'Hot take: introduction computer science education should be about capturing interests, not teaching foundations.'
+          }
+        </P>
+      </BlogEntry>
+      <BlogEntry date="05/06/2021">
+        <P>
+          {
+            'On a scale of 1 to the american education system, how elitist is it to be bewildered by the fact that 90% of people at a concert in Seattle had a hard time recognizing Smells Like Teen Spirit?'
+          }
+        </P>
       </BlogEntry>
       <BlogEntry date="05/03/2023">
-        <P>{'The churning of a near empty lemonade machine. A low fan hums as he worked quietly during the closing shift.'}</P>
+        <P>
+          {
+            'The churning of a near empty lemonade machine. A low fan hums as he worked quietly during the closing shift.'
+          }
+        </P>
       </BlogEntry>
     </>
   );
