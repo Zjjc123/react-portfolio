cda4a7d2395cd4631aaae0db1d619cd7769f548a
Jason Zhang <zjjc123@hotmail.com>
2023-05-27 23:21:51 -0700

    Mobile Friendlier

--- app/page.tsx
+++ app/page.tsx
@@ -5,16 +5,16 @@ import Portfolio from '../components/Portfolio';
 export default function Home() {
   return (
     <div className="bg-white text-black tracking-wide">
-      <div className="grid grid-cols-3 gap-4 my-8">
-        <div className="px-8">
+      <div className="flex flex-col md:flex-row my-8">
+        <div className="mb-16 md:mb-0 flex-1 px-8">
           <div className="uppercase text-xl tracking-widest mb-4">About</div>
           <Personal />
         </div>
-        <div className="px-8">
+        <div className="mb-16 md:mb-0 md:flex-1 px-8">
           <div className="uppercase text-xl tracking-widest mb-4">PORTFOLIO</div>
           <Portfolio />
         </div>
-        <div className="px-8">
+        <div className="md:flex-1 px-8">
           <p className="uppercase text-xl tracking-widest mb-4">Blog</p>
           <Blog />
         </div>
