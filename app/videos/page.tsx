47c05432b58c9dfe4155467544fee28aa8b24ed0
Jason Zhang <zjjc123@hotmail.com>
2023-06-11 20:55:47 -1000

    More Videos

--- app/videos/page.tsx
+++ app/videos/page.tsx
@@ -6,12 +6,22 @@ type Props = {};
 export default function VideoPage({}: Props) {
   return (
     <div>
-        <div className="flex flex-col justify-center my-8">
-            <VideoModule title="Ude Dil Levitating - UW Kahaani" src="kxFhZwQ3eu0" />
-            <VideoModule title="VEX Worlds Reveal - 917M" src="lWE1iFQBnmM" />
-            <VideoModule title="PSA 2020 ILC Winner - HOSA" src="iL9gJoI2TR4" />
-        </div>
+      <div className="flex flex-col justify-center my-8">
+        <VideoModule
+          title="EUPHORIA X Bollywood - UW Kahaani"
+          src="PMSljieUlmE"
+        />
+        <VideoModule
+          title="Ude Dil Levitating - UW Kahaani"
+          src="kxFhZwQ3eu0"
+        />
+        <VideoModule title="VEX Worlds Reveal - 917M" src="lWE1iFQBnmM" />
+        <VideoModule
+          title="Viral Vision | Breakthrough Junior Challenge"
+          src="8PgU2d7qOgM"
+        />
+        <VideoModule title="PSA 2020 ILC Winner - HOSA" src="iL9gJoI2TR4" />
+      </div>
     </div>
-    
   );
 }
